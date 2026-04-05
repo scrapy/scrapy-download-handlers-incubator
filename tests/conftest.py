faae789f7b28220e1227f7dea8cb5a4a86322ace
Andrey Rakhmatullin <wrar@wrar.name>
2026-04-05 16:05:14 +0500

    Fix tests on Windows.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
+from scrapy.utils.reactor import set_asyncio_event_loop_policy
 
 from tests.mockserver.http import MockServer
 
@@ -14,3 +15,8 @@ if TYPE_CHECKING:
 def mockserver() -> Generator[MockServer]:
     with MockServer() as mockserver:
         yield mockserver
+
+
+def pytest_configure(config):
+    # Needed on Windows to switch from proactor to selector for Twisted reactor compatibility.
+    set_asyncio_event_loop_policy()
